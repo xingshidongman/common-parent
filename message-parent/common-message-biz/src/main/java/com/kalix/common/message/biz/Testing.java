183ba9c9f0c58dddd393174a6dc85d6043c6b1cd
minikiller <minikiller@qq.com>
2018-06-06 23:29:59 +0800

    add quartz testing code

---
+++ message-parent/common-message-biz/src/main/java/com/kalix/common/message/biz/Testing.java
@@ -0,0 +1,11 @@
+package com.kalix.common.message.biz;
+
+/**
+ * Created by minikiller on 2018/6/6.
+ */
+public class Testing {
+    public String sayHello(String name) {
+        System.out.println("hello");
+        return "Hello " + name + "!";
+    }
+}
